01b4b11289701eb9426f3fab1e92cfc93c8df2ec
kikro1 <bogdash.voitencov.2008@gmail.com>
2023-12-26 16:56:22 +0000

    fullcode

---
+++ js/task-4.js
@@ -0,0 +1,12 @@
+const getTotalBalanceByGender = (users, gender) => {
+    return users
+      .filter((user) => user.gender === gender)
+      .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+  };
+  
+  const maleTotalBalance = getTotalBalanceByGender(allUsers, "male");
+  console.log(maleTotalBalance); // 12053
+  
+  const femaleTotalBalance = getTotalBalanceByGender(allUsers, "female");
+  console.log(femaleTotalBalance); // 8863
+  
